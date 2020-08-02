dc9387b18424cfdf0654378e6d51820889741c08
alex yu <alex.yu@danielwellington.com>
2020-08-02 22:30:55 +0800

    insertion sort v3.1

--- sort/insertion/v3/main.go
+++ sort/insertion/v3/main.go
@@ -9,16 +9,16 @@ func main() {
 }
 
 // 从后向前实现插入排序
+// 小优化版
 func InsertionSort(nums []int) {
 	n := len(nums)
 	// 循环不变量: nums[0,i)未排序, nums[i...n)已排序
 	for i := n - 2; i >= 0; i-- {
-		for j := i; j < n-1; j++ {
-			if nums[j] > nums[j+1] {
-				nums[j], nums[j+1] = nums[j+1], nums[j]
-			} else {
-				break
-			}
+		e := nums[i]
+		j := 0
+		for j = i; j < n-1 && nums[j+1] < e; j++ {
+			nums[j] = nums[j+1]
 		}
+		nums[j] = e
 	}
 }
