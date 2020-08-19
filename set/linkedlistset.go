0c7d4be0324eda85f8c487ab07c8bf0cc6977ce5
alex yu <alex.yu@danielwellington.com>
2020-08-19 22:35:39 +0800

    linked list set

--- set/linkedlistset.go
+++ set/linkedlistset.go
@@ -1,4 +1,38 @@
 package set
 
+import (
+	"github.com/corrots/data-structures/linkedlist/linkedlist"
+)
+
 type LinkedListSet struct {
+	list *linkedlist.LinkedList
+}
+
+func NewLinkedListSet() *LinkedListSet {
+	return &LinkedListSet{
+		list: linkedlist.New(),
+	}
+}
+
+// 基于链表实现的Set，需先查重
+func (s *LinkedListSet) Add(i interface{}) {
+	if !s.Contains(i) {
+		s.list.AddFirst(i)
+	}
+}
+
+func (s *LinkedListSet) Remove(i interface{}) {
+	s.list.Remove(i.(int))
+}
+
+func (s *LinkedListSet) Contains(i interface{}) bool {
+	return s.list.Contains(i)
+}
+
+func (s *LinkedListSet) Len() int {
+	return s.list.Len()
+}
+
+func (s *LinkedListSet) IsEmpty() bool {
+	return s.list.IsEmpty()
 }
