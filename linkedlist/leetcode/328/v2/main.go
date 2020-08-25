c04145866f24710f6b700bbed4cce5dc5072caa9
alex yu <alex.yu@danielwellington.com>
2020-08-25 23:13:33 +0800

    leetcode #328 v2

---
+++ linkedlist/leetcode/328/v2/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	head := CreateLinkedList(nums, len(nums))
+	PrintLinkedList(head)
+	o := oddEvenList(head)
+	PrintLinkedList(o)
+}
+
+// https://leetcode-cn.com/problems/odd-even-linked-list/
+func oddEvenList(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	odd := head
+	even := head.Next
+	evenHead := even
+	for even != nil && even.Next != nil {
+		odd.Next = even.Next
+		odd = odd.Next
+		even.Next = odd.Next
+		even = even.Next
+	}
+	odd.Next = evenHead
+	return head
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewListNode(val int) *ListNode {
+	return &ListNode{Val: val}
+}
+
+func CreateLinkedList(nums []int, n int) *ListNode {
+	if n == 0 {
+		return nil
+	}
+	head := NewListNode(nums[0])
+	cur := head
+	for i := 1; i < n; i++ {
+		cur.Next = NewListNode(nums[i])
+		cur = cur.Next
+	}
+	return head
+}
+
+func PrintLinkedList(head *ListNode) {
+	cur := head
+	for cur != nil {
+		fmt.Printf("%d->", cur.Val)
+		cur = cur.Next
+	}
+	fmt.Printf("nil\n")
+}
