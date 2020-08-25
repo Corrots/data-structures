48c7a135c35bfdd92512c1d3f782d16b43c11444
alex.yu <alex.yu@danielwellington.com>
2020-08-25 18:06:31 +0800

    leetcode #83

---
+++ linkedlist/leetcode/83/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 1, 1}
+	head := CreateLinkedList(nums, len(nums))
+	PrintLinkedList(head)
+	mod1 := deleteDuplicates(head)
+	PrintLinkedList(mod1)
+	//nums1 := []int{1, 1, 2, 3, 3}
+
+}
+
+// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	cur := head
+	for cur != nil {
+		next := cur.Next
+		if next != nil && cur.Val == next.Val {
+			delNode := cur.Next
+			cur.Next = delNode.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewListNode(val int) *ListNode {
+	return &ListNode{Val: val}
+}
+
+func CreateLinkedList(nums []int, n int) *ListNode {
+	if n == 0 {
+		return nil
+	}
+	head := NewListNode(nums[0])
+	cur := head
+	for i := 1; i < n; i++ {
+		cur.Next = NewListNode(nums[i])
+		cur = cur.Next
+	}
+	return head
+}
+
+func PrintLinkedList(head *ListNode) {
+	cur := head
+	for cur != nil {
+		fmt.Printf("%d->", cur.Val)
+		cur = cur.Next
+	}
+	fmt.Printf("nil\n")
+}
