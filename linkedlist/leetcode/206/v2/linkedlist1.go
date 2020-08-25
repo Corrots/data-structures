73523a01f6952e811ea0d61d58eb0f7a362b096b
alex.yu <alex.yu@danielwellington.com>
2020-08-25 15:07:06 +0800

    leetcode #206

--- linkedlist/leetcode/206/v2/linkedlist1.go
+++ linkedlist/leetcode/206/v2/linkedlist1.go
@@ -1,11 +1,16 @@
-package leetcode
+package v2
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
 // 反转链表-递归实现
-func reverseList1(head *ListNode) *ListNode {
+func reverseList(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
-	rev := reverseList1(head.Next)
+	rev := reverseList(head.Next)
 	head.Next.Next = head
 	head.Next = nil
 	return rev
