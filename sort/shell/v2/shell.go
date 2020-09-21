ed03192ea49b501fdcf5b1e9b2a03fe929202857
alex.yu <alex.yu@danielwellington.com>
2020-09-21 15:16:03 +0800

    shell sort modify steps

--- sort/shell/v2/shell.go
+++ sort/shell/v2/shell.go
@@ -1,5 +1,26 @@
 package v2
 
+// 调整步长序列h
+func ShellSort2(nums []int) {
+	// 确定h的最大值
+	h := 1
+	for h < len(nums)/3 {
+		h = h*3 + 1
+	}
+	for h >= 1 {
+		// 对nums[h,n)进行插入排序
+		for i := h; i < len(nums); i++ {
+			tmp := nums[i]
+			j := 0
+			for j = i; j-h >= 0 && nums[j-h] > tmp; j -= h {
+				nums[j] = nums[j-h]
+			}
+			nums[j] = tmp
+		}
+		h /= 3
+	}
+}
+
 func ShellSort(nums []int) {
 	h := len(nums) / 2
 	for h >= 1 {
--- sort/shell/v2/test/main.go
+++ sort/shell/v2/test/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"math"
-	"sort"
-
-	v4 "github.com/corrots/data-structures/sort/merge/v4"
 
 	"github.com/corrots/data-structures/sort/helper"
 
@@ -17,14 +14,13 @@ func main() {
 	nums := []int{4, 3, 2, 7, 5, 1, 6, 8}
 	v1.ShellSort(nums)
 	fmt.Println(nums)
-	count := 1000000
+	count := 5000000
 	nums1 := helper.GenerateRandArray(count, 0, math.MaxInt64)
 	nums2 := make([]int, count)
 	copy(nums2, nums1)
 	//v1.ShellSort(nums1)
 	t1 := helper.TestSort(nums1, v2.ShellSort)
-	fmt.Printf("ShellSort v2: %d ms\n", t1)
-	t2 := helper.TestSort(nums2, v4.MergeSort)
-	fmt.Printf("MergeSort: %d ms\n", t2)
-	sort.Ints()
+	fmt.Printf("ShellSort: %d ms\n", t1)
+	t2 := helper.TestSort(nums2, v2.ShellSort2)
+	fmt.Printf("ShellSort2: %d ms\n", t2)
 }
