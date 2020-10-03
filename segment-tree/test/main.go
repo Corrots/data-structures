29ef21b0de13be66218350c9f1d45705198ace3d
alex.yu <alex.yu@danielwellington.com>
2020-10-03 15:52:49 +0800

    segment tree

--- segment-tree/test/main.go
+++ segment-tree/test/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	segmenttree "github.com/corrots/data-structures/segment-tree"
+)
+
+func main() {
+	nums := []int{-2, 0, 3, -5, 2, -1}
+	segTree := segmenttree.NewSegmentTree(nums, sumFunc)
+	fmt.Println(segTree)
+}
+
+func sumFunc(v1, v2 int) int {
+	return v1 + v2
 }
