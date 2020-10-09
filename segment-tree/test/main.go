99a843c969762c76e071c3e490b390e02e4ca9e9
alex.yu <alex.yu@danielwellington.com>
2020-10-09 13:08:54 +0800

    segment tree query

--- segment-tree/test/main.go
+++ segment-tree/test/main.go
@@ -10,6 +10,9 @@ func main() {
 	nums := []int{-2, 0, 3, -5, 2, -1}
 	segTree := segmenttree.NewSegmentTree(nums, sumFunc)
 	fmt.Println(segTree)
+	fmt.Println(segTree.Query(0, 2))
+	fmt.Println(segTree.Query(2, 5))
+	fmt.Println(segTree.Query(0, 5))
 }
 
 func sumFunc(v1, v2 int) int {
