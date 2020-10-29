c4924936db85e213a0518ad91cfba649a79979f9
alex.yu <alex.yu@danielwellington.com>
2020-10-29 17:40:21 +0800

    AVL Tree

--- AVL/avl.go
+++ AVL/avl.go
@@ -9,6 +9,32 @@ func NewAVL() *AVL {
 	return &AVL{}
 }
 
+// 判断该二叉树是否是一棵二叉搜索树(通过中序遍历判断)
+func (avl *AVL) IsBST() bool {
+	var keys []int
+	var inOrder func(*TreeNode)
+	inOrder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		inOrder(node.left)
+		keys = append(keys, node.key)
+		inOrder(node.right)
+	}
+	inOrder(avl.root)
+	for i := 1; i < len(keys); i++ {
+		if keys[i-1] > keys[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// 判断该二叉树是否为平衡二叉树
+func (avl *AVL) IsBalanced() bool {
+	return avl.root.isBalanced()
+}
+
 func (avl *AVL) Len() int {
 	return avl.size
 }
